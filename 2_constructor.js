4e786ae5d313680b7d5a2784e87ec23c48e2e98d
Vishesh Singh <VisheshSingh16@gmail.com>
2020-12-13 18:21:49 -0600

    methods if objects

--- 2_constructor.js
+++ 2_constructor.js
@@ -2,10 +2,17 @@ function Book(title, author, year) {
   this.title = title;
   this.author = author;
   this.year = year;
+
+  this.getDetails = function () {
+    console.log(
+      `${this.title} is authored by ${this.author} and year is ${this.year}`
+    );
+  };
 }
 
 const book1 = new Book('Book 1', 'John Doe', 2013);
 const book2 = new Book('Book 2', 'Jane Doe', 2016);
 
 console.log(book1);
-console.log(book2);
+console.log(book2.getDetails);
+book2.getDetails();
