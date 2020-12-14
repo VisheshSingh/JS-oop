aa8de1f1073d0c26c1852f40d1e6ad516baeed86
Vishesh Singh <VisheshSingh16@gmail.com>
2020-12-13 18:17:14 -0600

    basic object literals

---
+++ 1_basic_literals.js
@@ -0,0 +1,14 @@
+const book1 = {
+  title: 'Book 1',
+  author: 'John Doe',
+  year: 2013,
+};
+
+const book2 = {
+  title: 'Book 2',
+  author: 'Jane Doe',
+  year: 2016,
+};
+
+console.log(book1);
+console.log(book2.title, book2.author, book2.year);
